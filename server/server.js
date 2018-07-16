3504604911d1f5077c45427f18d414060720813c
Frederik Wouters <woutersf@gmail.com>
2018-07-16 11:26:36 +0200

    Add public file hosting

--- server/server.js
+++ server/server.js
@@ -6,10 +6,13 @@ const app = express();
 // set listening port
 app.set('port', 8080);
 
+app.use(express.static('app/dist'));
+
 // start listening
 const server = app.listen(app.get('port'), () => {
   console.log(`Express is listening on port ${app.get('port')}`);
 });
+
 
 // Export the server
 module.exports.server = server;
